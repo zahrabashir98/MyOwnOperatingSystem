483b31f7f24652212f56d8546953b856c72b03c6
zahra <zahra_9877@yahoo.com>
2019-01-08 00:49:35 +0330

    Add InterruptHandler class

--- interrupts.cpp
+++ interrupts.cpp
@@ -2,6 +2,28 @@
 
 void printf(char* str);
 
+
+
+InterruptHandler :: InterruptHandler(uint8_t interruptNumber, InterruptManager* interruptManager)
+{
+    this->interruptNumber = interruptNumber;
+    this->interruptManager = interruptManager;
+    interruptManager->handlers[interruptNumber] = this;
+}
+
+InterruptHandler :: ~InterruptHandler()
+{
+    if (interruptManager->handlers[interruptNumber] == this)
+        interruptManager->handlers[interruptNumber] = 0;
+}
+
+uint32_t InterruptHandler :: HandleInterrupt(uint32_t esp)
+{
+    return esp;
+}
+
+
+
 InterruptManager :: GateDescriptor InterruptManager:: interruptDescriptorTable[256];
 
 InterruptManager* InterruptManager :: ActiveInterruptManager = 0;
@@ -32,8 +54,10 @@ InterruptManager :: InterruptManager(GlobalDescriptorTable* gdt)
     const uint8_t IDT_INTERRUPT_GATE =0xE; // type
 
 
-    for (uint16_t i=0; i<256;i++)
+    for (uint16_t i=0; i<256;i++){
+        handlers[i] = 0;
         SetInterruptDescriptorTableEntry(i, CodeSegment, &IgnoreInterruptRequest, 0,IDT_INTERRUPT_GATE);
+    }
     
     SetInterruptDescriptorTableEntry(0x20, CodeSegment, &HandleInterruptRequest0x00, 0,IDT_INTERRUPT_GATE);
     SetInterruptDescriptorTableEntry(0x21, CodeSegment, &HandleInterruptRequest0x01, 0,IDT_INTERRUPT_GATE);
@@ -95,10 +119,31 @@ uint32_t InterruptManager :: handleInterrupt(uint8_t interruptNumber, uint32_t e
 
 }
 
-// here we send answer to interrupt (we dont want os to halt here)
+// here we send answer to  hardware interrupt (we dont want os to halt here)
 uint32_t InterruptManager :: DoHandleInterrupt(uint8_t interruptNumber, uint32_t esp)
 {
-    printf("INTERRUPT");
+
+    if (handlers[interruptNumber]!=0)
+    {
+        esp = handlers[interruptNumber]->HandleInterrupt(esp);
+    }
+
+    else if (interruptNumber != 0x20){
+        // if it is not timer interrupt
+        char* foo = "UNHANDLED INTERRUPT 0x00";
+        char* hex = "0123456789ABCDEF";
+        foo[22] = hex[(interruptNumber >> 4) & 0x0F];
+        foo[23] = hex[interruptNumber & 0x0F];
+        printf(foo);
+    }
+    //  this case we have to send answer
+    if (0x20 <= interruptNumber < 0x30){
+        picMasterCommand.Write(0x20);
+
+        if (0x28 <= interruptNumber){
+            // slaves only
+            picSalveCommand.Write(0x20);}
+    }
     return esp;
 
 }
