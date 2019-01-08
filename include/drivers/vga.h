e2783161ba49f6a085d36398a25c97665b3cb844
zahra <zahra_9877@yahoo.com>
2019-01-08 22:39:59 +0330

    Add vga.cpp and vga.h files

---
+++ include/drivers/vga.h
@@ -0,0 +1,52 @@
+
+#ifndef __MYOS__DRIVERS__VGA_H
+#define __MYOS__DRIVERS__VGA_H
+
+#include <common/types.h>
+#include <hardwarecommunication/port.h>
+#include <drivers/driver.h>
+
+namespace myos
+{
+    namespace drivers
+    {
+        
+        class VideoGraphicsArray
+        {
+        protected:
+            hardwarecommunication::Port8Bit miscPort;
+            hardwarecommunication::Port8Bit crtcIndexPort;
+            hardwarecommunication::Port8Bit crtcDataPort;
+            hardwarecommunication::Port8Bit sequencerIndexPort;
+            hardwarecommunication::Port8Bit sequencerDataPort;
+            hardwarecommunication::Port8Bit graphicsControllerIndexPort;
+            hardwarecommunication::Port8Bit graphicsControllerDataPort;
+            hardwarecommunication::Port8Bit attributeControllerIndexPort;
+            hardwarecommunication::Port8Bit attributeControllerReadPort;
+            hardwarecommunication::Port8Bit attributeControllerWritePort;
+            hardwarecommunication::Port8Bit attributeControllerResetPort;
+            // sends intialziation codes to the ports
+            void WriteRegisters(common::uint8_t* registers);
+            // gives the correct offset for the segment we want to use
+            common::uint8_t* GetFrameBufferSegment();
+            
+            virtual common::uint8_t GetColorIndex(common::uint8_t r, common::uint8_t g, common::uint8_t b);
+            
+            
+        public:
+            VideoGraphicsArray();
+            ~VideoGraphicsArray();
+            
+            virtual bool SupportsMode(common::uint32_t width, common::uint32_t height, common::uint32_t colordepth);
+            virtual bool SetMode(common::uint32_t width, common::uint32_t height, common::uint32_t colordepth);
+            //puts that color rgb to that coordinate x y
+            virtual void PutPixel(common::uint32_t x, common::uint32_t y,  common::uint8_t r, common::uint8_t g, common::uint8_t b);
+            // just you select index
+            virtual void PutPixel(common::uint32_t x, common::uint32_t y, common::uint8_t colorIndex);
+            
+        };
+        
+    }
+}
+
+#endif
